60800f56ba7a75915ee4ebf3c97fa635a09e5ce1
Urantij <maximumuranti@gmail.com>
2023-03-06 08:50:52 +0700

    Поправит модель плейбекдата

--- Pubsub/Payloads/Playback/PlaybackData.cs
+++ Pubsub/Payloads/Playback/PlaybackData.cs
@@ -11,23 +11,34 @@ public class PlaybackData
 {
     /// <summary>
     /// DateTimeOffset.FromUnixTimeSeconds
+    /// Доступно, если тип "stream-up" "stream-down"
     /// </summary>
     [JsonPropertyName("server_time")]
-    public long ServerTime { get; set; }
+    public long? ServerTime { get; set; }
 
+    /// <summary>
+    /// Доступно, если тип "stream-up"
+    /// </summary>
     [JsonPropertyName("play_delay")]
     public int? PlayDelay { get; set; }
 
     /// <summary>
-    /// "stream-up" "stream-down"
+    /// Доступно, если тип "viewcount"
+    /// </summary>
+    [JsonPropertyName("viewers")]
+    public int? Viewers { get; set; }
+
+    /// <summary>
+    /// "stream-up", "stream-down", "viewcount"
     /// </summary>
     [JsonPropertyName("type")]
     public string Type { get; set; }
 
-    public PlaybackData(long serverTime, int? playDelay, string type)
+    public PlaybackData(long? serverTime, int? playDelay, int? viewers, string type)
     {
         ServerTime = serverTime;
         PlayDelay = playDelay;
+        Viewers = viewers;
         Type = type;
     }
 }
